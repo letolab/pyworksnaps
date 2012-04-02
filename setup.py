bd6da1e5f7ea3a2cd1e166ed3cb60bc166480bc0
Alex Berezovskiy <letoosh@gmail.com>
2012-04-02 23:52:33 +0100

    Prepared for PyPI

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from distutils.core import setup
 setup(
     name='pyworksnaps',
     version='0.1',
+    description='Python module for Worksnaps API',
+    author='LetoLab Ltd',
+    author_email='team@letolab.com',
+    url='https://github.com/letolab/pyworksnaps',
     py_modules=['pyworksnaps'],
 )
